6312313050d4ac3ed0080cb0754628eeb7ebf451
Gabe D <gdomingu@github.com>
2022-03-09 14:04:35 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-
 # Figure out the absolute path of the dotfiles directory
 
 DOTFILESDIR="$(pwd -P)"
